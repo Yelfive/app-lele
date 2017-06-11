0ae29c865a5b8ee705949564bce529950d7b4e24
Felix <1449704765@qq.com>
2017-06-11 08:53:14 +0800

    add vendor fk/ease-mob modify ErrorHandler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,6 +7,7 @@ use App\Components\HttpStatusCode;
 use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Exceptions\PostTooLargeException;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Validation\ValidationException;
 
@@ -95,5 +96,24 @@ class Handler extends ExceptionHandler
         return redirect()->back()->withInput(
             $request->input()
         )->withErrors($errors);
+    }
+
+    protected function prepareResponse($request, Exception $e)
+    {
+        if ($request->expectsJson()) {
+            $result = new ApiResult();
+            /** @var \Illuminate\Routing\ResponseFactory $response */
+            $response = response();
+            if ($e instanceof PostTooLargeException) {
+                $result->code(HttpStatusCode::CLIENT_REQUEST_ENTITY_TOO_LARGE)
+                    ->message($e->getMessage() ?: '图片或文件不能超过' . ini_get('post_max_size')); // TODO: i18n
+            } else {
+                $result->code(HttpStatusCode::SERVER_INTERNAL_ERROR)
+                    ->message($e->getMessage() ?: '服务器内部错误'); // TODO: i18n
+            }
+            return $response->json($result, HttpStatusCode::getStatusCode($result->code));
+        } else {
+            return parent::prepareResponse($request, $e);
+        }
     }
 }
