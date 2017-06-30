ea33df2ec69a08ae294ef2e51fe13827a7fb9d59
Felix <1449704765@qq.com>
2017-06-30 14:36:42 +0800

    Code rewrite

--- app/Http/Controllers/Friends/ListController.php
+++ app/Http/Controllers/Friends/ListController.php
@@ -16,34 +16,32 @@ use Illuminate\Support\Facades\Auth;
 class ListController extends ApiController
 {
 
-    public function index(Request $request)
-    {
-        /** @var LengthAwarePaginator $paginator */
-        $paginator = Model::from('user_friends as uf')
-            ->select([
-                'u' => [
-                    'id', 'nickname', 'state_code', 'mobile', 'avatar', 'account', 'im_account', 'sex',
-                    'city_name', 'city_code', 'age', 'it_says', 'address', 'created_at', 'updated_at'
-                ],
-                'uf' => ['friend_nickname']
-            ])
-            ->where('created_by', Auth::id())
-            ->leftJoin('user as u', 'u.id', '=', 'uf.created_by')
-            ->where('uf.created_by', Auth::id())
-            ->orderBy('uf.id', 'DESC')
-            ->paginate($request->get('per_page', 1000), $this->listFields());
-
-        $this->result
-            ->message('获取好友列表成功')
-            ->extend($paginator->toFKStyle());
-    }
 
     protected function listFields()
     {
         return [
-            'uf.id', 'friend_id', 'friend_nickname',
-            'u.avatar', 'u.mobile', 'u.city_name', 'u.it_says'
+            'u' => [
+                'id', 'nickname', 'state_code', 'mobile', 'avatar', 'account', 'im_account', 'sex',
+                'city_name', 'city_code', 'age', 'it_says', 'address', 'created_at', 'updated_at'
+            ],
+            'uf' => ['friend_nickname']
         ];
+    }
+
+    public function index(Request $request)
+    {
+        /** @var LengthAwarePaginator $paginator */
+        $paginator = Model::from('user_friends as uf')
+            ->select($this->listFields())
+            ->where('created_by', Auth::id())
+            ->leftJoin('user as u', 'u.id', '=', 'uf.created_by')
+            ->where('uf.created_by', Auth::id())
+            ->orderBy('uf.id', 'DESC')
+            ->paginate($request->get('per_page', 1000));
+
+        $this->result
+            ->message('获取好友列表成功')
+            ->extend($paginator->toFKStyle());
     }
 
 }
