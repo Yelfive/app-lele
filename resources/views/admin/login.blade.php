818ab183245bc373b0f5f86e502484167006f226
Felix <1449704765@qq.com>
2017-07-26 13:58:15 +0800

    fix: absolute url for css of view login

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -5,9 +5,9 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>登陆</title>
-    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="/css/app.css">
     {{--<link rel="stylesheet" href="css/bootstrap.css">--}}
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="/css/login.css">
 </head>
 <body>
 <form action="/admin/session" method="post">
